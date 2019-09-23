c2ef549a6ba1ac0099afe6f5388c82d831d4c2d5
Minho Lee <alsgkgk77@gmail.com>
2019-09-23 19:33:36 +0900

    알고리즘_백준_#14889_스타트와 링크

---
+++ boj_14889.java
@@ -0,0 +1,20 @@
+package samsung;
+
+import java.util.regex.Pattern;
+
+public class n14889 {
+	
+	public static void main(String[] args) {
+		String[] emails = {"d@co@m.org","a@abc.org","b@def.org","c@ghi.net"};
+		String namePattern = "[a-z.]+@[a-z]+.(com)?(net)?(org)?";
+		String domainPattern = "[a-z]+";
+
+		int answer = 0;
+		for (String email : emails) {
+			if(Pattern.matches(namePattern, email)) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+}
