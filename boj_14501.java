618631ff38ad3d85b4d47e87362fc8210940b1a5
Minho Lee <alsgkgk77@gmail.com>
2019-09-30 20:31:45 +0900

    알고리즘_백준_#14501_퇴사

---
+++ boj_14501.java
@@ -0,0 +1,52 @@
+package samsung;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N1 {
+	static int N;
+	static int[][] arr;
+	static int MAX = 0;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(bf.readLine());
+		arr = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int t = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+
+			arr[i][0] = t;
+			arr[i][1] = p;
+		}
+
+		solve(0, 0);
+		System.out.println(MAX);
+		bf.close();
+	}
+
+	static void solve(int x, int sum) {
+		//System.out.println("x : " + x + " , sum : " + sum);
+		if (x == N) {
+			if (sum > MAX) {
+				MAX = sum;
+			}
+			return;
+		}
+
+		// 현재 index + 일하는날의수
+		if (x + arr[x][0] <= N) {
+			solve(x + arr[x][0], sum + arr[x][1]);
+			solve(x + 1, sum);
+		} else {
+			solve(x + 1, sum);
+		}
+	}
+
+	// N까지만 유효, N+1로 넘어가면 out
+}
