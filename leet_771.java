535a85b6b5ee219228c91f22e85993b688557aa5
Minho Lee <alsgkgk77@gmail.com>
2019-12-03 23:30:03 +0900

    LeetCode_2019-12-03

---
+++ leet_771.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        
+        int result = 0;
+		
+		Map<Character, Integer> jewels = new HashMap<>();
+		for(int i=0; i<J.length(); i++) {
+			char c = J.charAt(i);
+			jewels.put(c, 1);
+		}
+		
+		for(int i=0; i<S.length(); i++) {
+			char c = S.charAt(i);
+			if(jewels.containsKey(c)) {
+				result++;
+			}
+		}
+        
+        return result;
+    }
+}
