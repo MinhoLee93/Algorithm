e41725cf891fcc7295aac47c8ed3cad665fe52e8
Minho Lee <alsgkgk77@gmail.com>
2019-12-15 23:41:06 +0900

    LeetCode_2019_12_15

---
+++ leet/easy/leet_66.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+		boolean flag = false;
+		digits[len - 1] = digits[len - 1] + 1;
+
+		for (int i = len - 1; i >= 0; i--) {
+			if (digits[i] >= 10) {
+				digits[i] = digits[i] - 10;
+				if (i == 0) {
+					flag = true;
+				} else {
+					digits[i - 1] += 1;
+				}
+			} else {
+				break;
+			}
+		}
+
+		if (flag) {
+			int[] result = new int[len + 1];
+			result[0] = 1;
+			for (int i = 1; i < len + 1; i++) {
+				result[i] = digits[i-1];
+			}
+			return result;
+		} else {
+			return digits;
+		}
+    }
+}
