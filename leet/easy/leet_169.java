865eb8a0db10a3ce5be5ca5b74bdaeb650398fdb
Minho Lee <alsgkgk77@gmail.com>
2020-01-06 23:59:09 +0900

    LeetCode_2020_01_06

---
+++ leet/easy/leet_169.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		int maxIndex = 0;
+		int maxNum = 0;
+        
+		for (int r : nums) {
+			if (hash.containsKey(r)) {
+				int temp = hash.get(r) + 1;
+				hash.put(r, temp);
+
+				if (temp > maxIndex) {
+					maxIndex = temp;
+					maxNum = r;
+				}
+			} else {
+				hash.put(r, 1);
+				if (1 > maxIndex) {
+					maxIndex = 1;
+					maxNum = r;
+				}
+			}
+		}
+        
+        return maxNum;
+    }
+}
