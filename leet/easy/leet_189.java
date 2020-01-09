775c17de2a5a47c0970c1555229469c6d0325869
Minho Lee <alsgkgk77@gmail.com>
2020-01-09 23:46:34 +0900

    LeetCode_2020_01_09

---
+++ leet/easy/leet_189.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (k == 0) {
+			return;
+		}
+
+		int tail = nums[nums.length - 1];
+		for (int i = nums.length - 1; i > 0; i--) {
+			nums[i] = nums[i - 1];
+		}
+		nums[0] = tail;
+		
+		rotate(nums, k - 1);
+    }
+}
