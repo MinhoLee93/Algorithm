7847d527a415d0b4d647986a325448a7140c27aa
Minho Lee <alsgkgk77@gmail.com>
2020-01-13 23:58:33 +0900

    LeetCode_2020_01_13

---
+++ leet/easy/leet_203.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        // head
+		while (head != null && head.val == val) {
+			head = head.next;
+		}
+
+		ListNode pointer = head;
+		while (pointer != null) {
+			// System.out.println(pointer.val);
+			if (pointer.next != null && pointer.next.val == val) {
+				pointer.next = pointer.next.next;
+			} else {
+				pointer = pointer.next;
+			}
+		}
+        
+        return head;
+    }
+}
