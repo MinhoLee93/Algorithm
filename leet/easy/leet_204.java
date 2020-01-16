50fcfea26af463915ea2c2d74a01a24f37848988
Minho Lee <alsgkgk77@gmail.com>
2020-01-16 23:38:04 +0900

    LeetCode_2020_01_16

---
+++ leet/easy/leet_204.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+        if(n==0 || n==1 || n==2){
+            return 0;
+        }
+        
+        int[] check = new int[n];
+		int result = n - 2;
+
+		for (int i = 2; i < check.length; i++) {
+
+			// 1 : already chekced
+			for (int j = 2; i * j < check.length; j++) {
+				if (check[i * j] == 1) {
+
+				} else {
+					check[i * j] = 1;
+					result--;
+				}
+			}
+		}
+        
+        return result;
+    }
+}
