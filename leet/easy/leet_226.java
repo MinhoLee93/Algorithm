5c520d9b6e74c2bf47c9552db73999eafb326937
Minho Lee <alsgkgk77@gmail.com>
2020-01-20 23:21:44 +0900

    LeetCode_2020_01_20

---
+++ leet/easy/leet_226.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        solve(root);
+        
+        return root;
+    }
+    
+    public void solve(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		TreeNode temp = node.left;
+		node.left = node.right;
+		node.right = temp;
+
+		solve(node.left);
+		solve(node.right);
+	}
+}
