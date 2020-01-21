7790279bc5b196c107564a21eaf82738dba1f3e3
Minho Lee <alsgkgk77@gmail.com>
2020-01-21 23:42:55 +0900

    LeetCode_2020_01_21

---
+++ leet/easy/leet_231.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+		}
+
+		int i = 0;
+		while (true) {
+			long temp = (long) Math.pow(2, i++);
+			if (temp == n) {
+				return true;
+			}
+
+			if (temp > n) {
+				return false;
+			}
+		}
+    }
+}
