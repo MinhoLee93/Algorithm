cff0f0de82fcfd002adf80b7f0981ab2531a2ea8
Minho Lee <alsgkgk77@gmail.com>
2020-01-24 00:13:34 +0900

    LeetCode_2020_01_23

---
+++ leet/easy/leet_234.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder base = new StringBuilder(); 
+        
+        while (head != null) {
+			base.append((char) head.val);
+			head = head.next;
+		}
+
+		if (base.toString().equals(base.reverse().toString())) {
+			return true;
+		} else {
+			return false;
+		}
+    }
+}
