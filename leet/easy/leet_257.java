249de48123082321dd3d234cc1626f0645bff86b
Minho Lee <alsgkgk77@gmail.com>
2020-01-30 23:39:23 +0900

    LeetCode_2020_01_30

---
+++ leet/easy/leet_257.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    static List<String> result;
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+        
+        result = new ArrayList<>();
+        solve("", root);
+        return result;
+    }
+    
+    public void solve(String path, TreeNode root) {
+		// System.out.println(path);
+
+		if (path.length() == 0) {
+			path += root.val;
+		} else {
+			path += "->" + root.val;
+		}
+
+		if (root.left == null && root.right == null) {
+			result.add(path);
+			return;
+		}
+
+		if (root.left != null) {
+			solve(path, root.left);
+		}
+
+		if (root.right != null) {
+			solve(path, root.right);
+		}
+
+	}
+}
