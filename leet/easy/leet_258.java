859140f81647071c55ac2c899aa7153fffbc9cca
Minho Lee <alsgkgk77@gmail.com>
2020-02-03 00:28:20 +0900

    LeetCode_2020_02_02

---
+++ leet/easy/leet_258.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class leet_258 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(addDigits(num));
+		
+	}
+
+	static int addDigits(int num) {
+		if (num < 10) {
+			return num;
+		}
+
+		int temp = 0;
+		while (num >= 10) {
+			temp += num % 10;
+			num = num / 10;
+		}
+		
+		temp = temp + num;
+		return addDigits(temp);
+	}
+}
