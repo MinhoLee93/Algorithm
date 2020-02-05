17825e0746a9153e41e19fde5dd0dbcbfdf3a7db
Minho Lee <alsgkgk77@gmail.com>
2020-02-05 18:06:18 +0900

    LeetCode_2020_02_05

---
+++ leet/easy/leet_278.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        long start = 1;
+		long end = n;
+		while (start < end) {
+			long c = (start + end) / 2;
+			if (!isBadVersion((int) c)) {
+				start = (int) c + 1;
+			} else {
+				end = (int) c;
+			}
+		}
+
+		return (int)start;
+    }
+}
