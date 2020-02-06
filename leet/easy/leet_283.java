61e575f7b3fa1feb7fd37a6586bbf66a144ff2fe
Minho Lee <alsgkgk77@gmail.com>
2020-02-06 23:38:16 +0900

    LeetCode_2020_02_06

---
+++ leet/easy/leet_283.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int cnt = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				for (int j = i; j < nums.length - 1; j++) {
+					nums[j] = nums[j + 1];
+				}
+				nums[nums.length - 1] = 0;
+				i--;
+			}
+
+			cnt++;
+			if (cnt == nums.length) {
+				break;
+			}
+		}
+    }
+}
