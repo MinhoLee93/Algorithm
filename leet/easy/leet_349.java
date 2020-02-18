658f51e7ff8a81babd9eeaee07831ee5565176a0
Minho Lee <alsgkgk77@gmail.com>
2020-02-18 21:45:08 +0900

    LeetCode_2020_02_18

---
+++ leet/easy/leet_349.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> m1 = new HashMap<>();
+
+		for (int n1 : nums1) {
+			if (!m1.containsKey(n1)) {
+				m1.put(n1, 1);
+			}
+		}
+
+		List<Integer> list = new ArrayList<>();
+
+		for (int n2 : nums2) {
+			if (m1.containsKey(n2)) {
+				list.add(n2);
+				m1.remove(n2);
+			}
+		}
+
+		int[] result = new int[list.size()];
+		int index = 0;
+		for (int i : list) {
+			result[index++] = i;
+		}
+        
+        return result;
+    }
+}
