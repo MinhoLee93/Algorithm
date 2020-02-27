dd1bf4ef0cee2f15889007c5cf447eb03bb2dd82
Minho Lee <alsgkgk77@gmail.com>
2020-02-27 23:13:26 +0900

    LeetCode_2020_02_27

--- leet/easy/leet_392.java
+++ leet/easy/leet_392.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int n = s.length(), m = t.length();
-        
-        int j = 0;
-        for(int i = 0;j<n&&i<m;i++){
-            if(s.charAt(j)==t.charAt(i)){
-                j++;
-            }
-        }
-        return j==n;
+        int check = -1;
+		for (int i = 0; i < s.length(); i++) {
+			String temp = s.substring(i, i + 1);
+
+			// 있을 경우
+			int find = t.indexOf(temp, check + 1);
+			if (find != -1) {
+				check = find;
+			} else {
+				return false;
+			}
+		}
+		return true;
     }
 }
---
+++ leet/easy/leet_409.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int result = 0;	
+		Map<String, Integer> hash = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			String temp = s.substring(i, i + 1);
+
+			if (hash.containsKey(temp)) {
+				hash.replace(temp, hash.get(temp) + 1);
+			} else {
+				hash.put(temp, 1);
+			}
+		}
+
+		Iterator<String> iter = hash.keySet().iterator();
+		while (iter.hasNext()) {
+			String key = iter.next();
+			if (hash.get(key) / 2 > 0) {
+				result += (hash.get(key) / 2) * 2;
+			}
+		}
+		
+		if(result != s.length()) {
+			return result + 1;
+		}else{
+            return result;
+        }
+    }
+}
