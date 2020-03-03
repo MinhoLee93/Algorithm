c8a34aa663920afa998bf9029f858f10791ae30f
Minho Lee <alsgkgk77@gmail.com>
2020-03-04 00:28:37 +0900

    LeetCode_2020_03_03

---
+++ leet/easy/leet_434.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countSegments(String s) {
+        
+        String[] tt = s.split("\\s+");
+		int cnt = 0;
+		for(String t : tt) {
+			if(t.length()>0) {
+				cnt++;
+			}
+		}
+        
+        return cnt;
+    }
+}
