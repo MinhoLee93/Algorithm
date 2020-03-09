414e09c7c11e6025329298a743938a94f1cd1b9b
Minho Lee <alsgkgk77@gmail.com>
2020-03-09 23:29:14 +0900

    LeetCode_2020_03_09

---
+++ leet/easy/leet_443.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+		sb.append(chars[0]);
+		int cnt = 1;
+        
+		for (int i = 1; i < chars.length; i++) {
+			// 다를 경우
+			if (chars[i] != sb.charAt(sb.length() - 1)) {
+				if (cnt > 1) {
+					sb.append(cnt);
+				}
+                cnt = 1;
+				sb.append(chars[i]);
+			} else {
+				cnt++;
+			}
+		}
+
+		if (cnt > 1) {
+			sb.append(cnt);
+		}
+
+		for (int i = 0; i < sb.length(); i++) {
+			chars[i] = sb.charAt(i);
+		}
+		
+		return sb.length();
+    }
+}
