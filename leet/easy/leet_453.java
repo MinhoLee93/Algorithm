0437df61bb897c78eb16dc38f17b0da736c593d7
Minho Lee <alsgkgk77@gmail.com>
2020-03-11 01:19:38 +0900

    LeetCode_2020_03_10

---
+++ leet/easy/leet_453.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int currentsum=0;
+        int len=nums.length;
+        int min=Integer.MAX_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+            currentsum+=nums[i];
+            min=Math.min(min,nums[i]);
+        }
+
+        return currentsum-len*min;
+    }
+}
