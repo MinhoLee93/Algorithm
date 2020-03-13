e85a4669db60b274a71e24030e1a180aca26ae51
Minho Lee <alsgkgk77@gmail.com>
2020-03-13 17:34:14 +0900

    LeetCode_2020_03_13

---
+++ leet/easy/leet_476.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findComplement(int num) {
+        
+        String binary = Integer.toBinaryString(num);
+		StringBuilder sb = new StringBuilder();
+        
+		for (int i = 0; i < binary.length(); i++) {
+			String c = binary.substring(i, i + 1);
+			
+			if(c.equals("0")) {
+				sb.append("1");
+			}else {
+				sb.append("0");
+			}
+		}
+		
+		return Integer.parseInt(sb.toString(), 2);
+    }
+}
---
+++ leet/easy/leet_482.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String licenseKeyFormatting(String S, int K) {
+      StringBuilder sb = new StringBuilder();
+
+		int cnt = 0;
+		for (int i = S.length() - 1; i >= 0; i--) {
+			char c = S.charAt(i);
+
+			if (c == '-') {
+				continue;
+			}
+
+            if (cnt == K) {
+				sb.append("-");
+				cnt = 0;
+			}
+            
+			sb.append(S.charAt(i));
+			cnt++;
+
+		}
+
+		return sb.reverse().toString().toUpperCase();
+    }
+}
