599eea0b265bef608dec3167a5e8d04d8b97e5e3
Minho Lee <alsgkgk77@gmail.com>
2020-03-18 22:51:55 +0900

    LeetCode_2020_03_18

--- leet/easy/leet_496.java
+++ leet/easy/leet_496.java
@@ -1,28 +1,32 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> nextGreaterElement = getNextGreaterElement(nums2);
-        
-        int[] greaterElements = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            greaterElements[i] = nextGreaterElement.containsKey(nums1[i]) ? 
-                                 nextGreaterElement.get(nums1[i]) : -1;
-        }
-        
-        return greaterElements;
-    }
-    
-    private HashMap<Integer, Integer> getNextGreaterElement(int[] nums) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for (int num: nums) {
-            while (stack.size() != 0 && stack.peek() < num) {
-                map.put(stack.pop(), num);
-            }
-            
-            stack.push(num);
-        }
-        
-        return map;
+		Map<Integer, Integer> hash = new HashMap<>();
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < nums2.length; i++) {
+			int temp = nums2[i];
+			// push if empty
+			if (stack.isEmpty()) {
+				stack.push(temp);
+			} else {
+				// pop
+				while (!stack.isEmpty() && stack.peek() < temp) {
+					int head = stack.pop();
+					hash.put(head, temp);
+				}
+				stack.push(temp);
+			}
+		}
+
+		int[] result = new int[nums1.length];
+		for (int i = 0; i < nums1.length; i++) {
+			int temp = nums1[i];
+			if (hash.containsKey(temp)) {
+				result[i] = hash.get(temp);
+			} else {
+				result[i] = -1;
+			}
+		}
+
+        return result;
     }
 }
---
+++ leet/easy/leet_500.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String[] s = { "QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM" };
+        List<String> result = new ArrayList<>();
+		for (String word : words) {
+			String temp = word.substring(0, 1).toUpperCase();
+			int index;
+			// 0
+			if (s[0].indexOf(temp) != -1) {
+				index = 0;
+			} else if (s[1].indexOf(temp) != -1) {
+				// 1
+				index = 1;
+			} else {
+				// 2
+				index = 2;
+			}
+
+			boolean check = true;
+			for (int i = 1; i < word.length(); i++) {
+				String t = word.substring(i, i + 1).toUpperCase();
+				// 동일 row에 존재하지 않는다면
+				if (s[index].indexOf(t) == -1) {
+					check = false;
+					break;
+				}
+			}
+			
+			// 동일 row에 존재한다면
+			if(check) {
+				result.add(word);
+			}
+		}
+        
+        return result.toArray(new String[result.size()]);
+    }
+}
